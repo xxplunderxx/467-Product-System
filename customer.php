be459dcafd57c7274c96ff36c707b20d7e57f6e4
xxplunderxx <71054045+xxplunderxx@users.noreply.github.com>
2022-04-09 15:30:53 -0500

    changed session shopping cart

--- customer.php
+++ customer.php
@@ -3,9 +3,33 @@
 include 'secrets.php';
 
 session_start();
-
-if(!(isset($_SESSION['cart']))) {
-	$_SESSION['cart'];
+if(isset($_POST['addToCart']))
+{       
+        if(isset($_SESSION['cart']))
+        {
+                // check item added to cart is already in the session
+                $item_array_id = array_column($_SESSION["shopping_cart"], "item_id");
+                if(!in_array($_POST["hidden_prod"], $item_array_id))    
+                {
+                        $count = count($_SESSION["shopping_cart"]);
+                        $item_array = array(
+                                'item_id' => $_POST["hidden_prod"],
+                                'item_quantity' => $_POST["quantity"]
+                        );
+                        $_SESSION["shopping_cart"][$count] = $item_array;   // store after previous items
+                }
+                else{   // we know item is ! stored in the session
+                        echo'Error item has already been added';
+                }
+        }
+        else    // we need to get data from the form
+        {
+                $item_array = array(
+                        'item_id' => $_POST["hidden_prod"],
+                        'item_quantity' => $_POST["quantity"]
+                );
+                $_SESSION["shopping_cart"][0] = $item_array;
+        } 
 }
 
 // boiler plate html
@@ -32,39 +56,6 @@ catch(PDOexception $e)
         echo "Connection to database failed: " . $e->getMessage();
 }
 
-//-*************************************************************************
-// This will add chosen quantities to the cart
-
-if(isset($_POST['addToCart'])) {
-
-	$prod = $_POST['prod'];
-	$quant = $_POST['quantity'];
-//	print_r($quant);
-
-	echo $prod;
-	//echo $quant;
-
-	// checking for invalid input
-	if($quant > 0 and is_numeric($quant)) {
-		if(isset($_SESSION['cart'][$prod])) {
-			$_SESSION['cart'][$prod] += $quant;
-		}
-		else {
-			$_SESSION['cart'][$prod] = $quant;
-		}
-	}
-	else {
-		echo "bad input";
-	}
-
-
-
-	print_r($_SESSION['cart']);
-
-	session_destroy();
-}
-
-
 //*************************************************************************/
 // This will query all of the products in order to display each available
 	$sql = "SELECT * FROM parts;";
@@ -79,19 +70,44 @@ if(isset($_POST['addToCart'])) {
 	{
 		$sql2 = "INSERT INTO Inventory(Num) VALUES(?);";
 		$prepared2 = $pdo2->prepare($sql2);
-		$prepared2->execute(array($item[0]));
+		$prepared2->execute(array($item[0]));   // item number from legacy_DB
 
 		$sql2 = "SELECT * FROM Inventory WHERE Num = ?;";
 		$prepared2 = $pdo2->prepare($sql2);
-		$prepared2->execute(array($item[0]));
+		$prepared2->execute(array($item[0]));   // item number from new_DB
 		$prod = $prepared2->fetch();
+
 		echo "<tr><td><img src=\"" . $item[4] . "\"></td><td>". $item[1] . "</td><td>$" . $item[2] . "</td><td>" . $item[3] . "lbs.</td><td>" . $prod[1] . "</td>";
 
-                echo "<td><input type=\"hidden\" name=\"prod\" value=\"$item[0]\" />Quantity:&nbsp;<input type=\"text\" name=\"quantity\"/><input type=\"submit\" name=\"addToCart\" value=\"Add to Cart\"/></td></tr>";
+                echo "<td><input type=\"hidden\" name=\"hidden_prod\" value=\"$item[0]\" />Quantity:&nbsp;<input type=\"text\" name=\"quantity\"/><input type=\"submit\" name=\"addToCart\" value=\"Add to Cart\"/></td></tr>";
 	}
 
 	echo "</form></table>";
 
+if(isset($_POST['addToCart'])) 
+{
+        $prod = $_POST['prod'];
+        $quant = $_POST['quantity'];
+
+        print_r($quant);
+
+        // checking for invalid input
+        /*if($quant > 0 and is_numeric($quant)) {
+                if(isset($_SESSION['cart'][$prod])) {
+                        $_SESSION['cart'][$prod] += $quant;
+                }
+                else {
+                        $_SESSION['cart'][$prod] = $quant;
+                }
+        }
+        else {
+                echo "bad input";
+        }
+        */
+        print_r($_SESSION['cart']);
+
+        session_destroy();
+}
 //}
 echo "</body>"
 

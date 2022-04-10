84c0d6073751a308ba847b23ca5dff4d052ad159
xxplunderxx <71054045+xxplunderxx@users.noreply.github.com>
2022-04-10 10:19:51 -0500

    current version

--- customer.php
+++ customer.php
@@ -78,14 +78,13 @@ catch(PDOexception $e)
 
 		echo "<tr><td><img src=\"" . $item[4] . "\"></td><td>". $item[1] . "</td><td>$" . $item[2] . "</td><td>" . $item[3] . "lbs.</td><td>" . $prod[1] . "</td>";
 
-                echo "<td><form action=\"http://students.cs.niu.edu/~z1886085/customer.php\" method=\"POST\"><input type=\"hidden\" name=\"prod_hidden\" value=\"$item[0]\" />Quantity:&nbsp;<input type=\"text\" name=\"quantity\"/><input type=\"submit\" name=\"addToCart\" value=\"Add to Cart\"/></td></tr>";
+                echo "<td><form action=\"http://students.cs.niu.edu/~z1892587/467-Product-System/customer.php\" method=\"POST\"><input type=\"hidden\" name=\"prod_hidden\" value=\"$item[0]\" />Quantity:&nbsp;<input type=\"text\" name=\"quantity\"/><input type=\"submit\" name=\"addToCart\" value=\"Add to Cart\"/></td></tr>";
 	}
         echo "</table>";
 
         if(!empty($_SESSION["shopping_cart"])) 
         {
-                print_r($_SESSION["shopping_cart"]);
-                echo "there is data";
+                print_r($_SESSION);
         }
         else{
                 echo "there is not data";

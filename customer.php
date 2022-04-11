062c134170e678cea4251c3ff09981b7551a2cb1
xxplunderxx <71054045+xxplunderxx@users.noreply.github.com>
2022-04-11 16:25:26 -0500

    added a link to checkout page

--- customer.php
+++ customer.php
@@ -6,6 +6,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Product System</title>
+        <a href="./checkout.php"><button>Checkout</button></a>
 </head>
 <body>
 <?php

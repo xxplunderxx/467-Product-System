34cbe7fa9be1d31a6f75755ad8a9959d68d60551
xxplunderxx <71054045+xxplunderxx@users.noreply.github.com>
2022-04-20 23:40:54 -0500

    secret file

---
+++ secrets.php
@@ -0,0 +1,15 @@
+<?php
+    $username = "student";
+    $password = "student";
+
+    $username2 = "z1892587";
+    $password2 = "2002Jul30";
+?>
+
+<?php
+    $username = "student";
+    $password = "student";
+
+    $username2 = "your_z#";
+    $password2 = "your_password";
+?>

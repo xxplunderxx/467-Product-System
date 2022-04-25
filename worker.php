99fe548102c57f657fb81e293b14a34985818988
Alexandra Alcantar <z1893793@hopper.cs.niu.edu>
2022-04-24 21:50:33 -0500

    Visual update of nav bar

--- worker.php
+++ worker.php
@@ -3,14 +3,13 @@
     <title>Orders Page for Workers</title>
         <style>
             h1{ text-align: center; }
-            h2{ text-align: center; }
-            p { text-align: center; }
             ul {
             list-style-type: none;
             margin: 0;
             padding: 0;
             overflow: hidden;
             background-color: #333;
+            border: 1px solid #555;
         }
         li {
             float: center;
@@ -22,17 +21,21 @@
             padding: 14px 16px;
             text-decoration: none;
         }
+        li a:hover:not(.active) {
+  background-color: #111;
+  color: white;
+}
         </style>
     </head>
     <body>
         <h1>Comp Name</h1>
         <ul>
             <li><a href="home.html">Home</a></li>
-            <li><a href="order_product.php">Inspection</a></li>
-            <li><a href="pending.php">Pending</a></li>
-            <li><a href="inbound.php">Inbound Products</a></li>
-            <li><a href="outbound.php">Outbound Products</a></li>
-            <li><a href="completed.php">Completed</a></li>
+            <li><a href="order_product.php">Order Inspection</a></li>
+            <li><a href="pending.php">Pending Orders</a></li>
+            <li><a href="inbound.php">Log Inbound Products</a></li>
+            <li><a href="outbound.php">View Outbound Products</a></li>
+            <li><a href="completed.php">Completed Orders</a></li>
         </ul>
     </body>
 </html>

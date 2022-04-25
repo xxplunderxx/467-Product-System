be45a7dd86bac57473752b73a211d9b9a5533997
xxplunderxx <71054045+xxplunderxx@users.noreply.github.com>
2022-04-25 18:40:27 -0500

    jake

--- secrets.php
+++ secrets.php
@@ -5,11 +5,3 @@
     $username2 = "z1892587";
     $password2 = "2002Jul30";
 ?>
-
-<?php
-    $username = "student";
-    $password = "student";
-
-    $username2 = "your_z#";
-    $password2 = "your_password";
-?>

205fac2412bf928edeb169e392316fe66a70a15e
xxplunderxx <71054045+xxplunderxx@users.noreply.github.com>
2022-05-02 18:54:26 -0500

    added login requirement

--- completed.php
+++ completed.php
@@ -1,7 +1,13 @@
 <?php 
     session_start(); //start session
-
     include('secrets.php');
+
+    // verify login 
+    $verified = false;
+    if ($_SESSION["login"][0] == "worker")
+    {
+        $verified = true;
+    }
     
     // Connecting to th legacy databse
     try { // if something goes wrong, an exception is thrown
@@ -45,26 +51,35 @@
             <th>Weight</th>
         </tr>
 <?php
-    // show all pending orders (on the Order_info table)
-    $sql = "SELECT * FROM Order_Info WHERE status = 'completed';";
-    foreach($pdo2->query($sql) as $item)
+    if($verified)
     {
-        echo "<tr>";
-            echo "<td>". $item[0] . "</td>";
-            echo "<td>". $item[1] . "</td>";
-            echo "<td>". $item[2] . "</td>";
-            echo "<td>". $item[3] . "</td>";
-            echo "<td>". $item[4] . "</td>";
-            echo "<td>". $item[5] . "</td>";
-            echo "<td>". $item[6] . "</td>";
-            echo "<td>". $item[7] . "</td>";
-            echo "<td><form action=\"http://students.cs.niu.edu/~z1892587/467-Product-System/order_product.php\" method=\"POST\">";
-                echo "<input type=\"hidden\" name=\"completed_hidden\" value=\"$item[0]\"/>";
-                echo "<input type=\"submit\" name=\"inspect_completed\" value=\"Inspect\"/>";
-            echo "</td></form>";
-        echo "</tr>";
+        // show all pending orders (on the Order_info table)
+        $sql = "SELECT * FROM Order_Info WHERE status = 'completed';";
+        foreach($pdo2->query($sql) as $item)
+        {
+            echo "<tr>";
+                echo "<td>". $item[0] . "</td>";
+                echo "<td>". $item[1] . "</td>";
+                echo "<td>". $item[2] . "</td>";
+                echo "<td>". $item[3] . "</td>";
+                echo "<td>". $item[4] . "</td>";
+                echo "<td>". $item[5] . "</td>";
+                echo "<td>". $item[6] . "</td>";
+                echo "<td>". $item[7] . "</td>";
+                echo "<td><form action=\"http://students.cs.niu.edu/~z1892587/467-Product-System/order_product.php\" method=\"POST\">";
+                    echo "<input type=\"hidden\" name=\"completed_hidden\" value=\"$item[0]\"/>";
+                    echo "<input type=\"submit\" name=\"inspect_completed\" value=\"Inspect\"/>";
+                echo "</td></form>";
+            echo "</tr>";
+        }
     }
-
+    elseif ($_SESSION["login"][0] == "default"){
+        echo "security status invalid: contact your System Adminstrator for upgraded privalages";
+    }
+    else{
+        echo "not logged in visit the login page";
+    }
+    
     // closes off html table open tag
     echo "<table/>";
 ?>

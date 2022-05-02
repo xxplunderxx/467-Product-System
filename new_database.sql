4bf25a02685f595674c53dd0cc8825201d683af5
julfinegan <89276926+julfinegan@users.noreply.github.com>
2022-05-02 13:29:17 -0500

    added weight brackets

--- new_database.sql
+++ new_database.sql
@@ -19,17 +19,34 @@ CREATE TABLE Order_Info(
 	cust_email	CHAR(40)	NOT NULL,
 	total_price	DECIMAL(8,2)	NOT NULL,
 	status		CHAR(12)	NOT NULL,
-	date		DATE	NOT NULL,
+	date		DATE	DEFAULT NOW(),
 	total_weight	DECIMAL(8,2)	NOT NULL,
 	PRIMARY KEY (Order_ID)
 );
 
 CREATE TABLE Order_Prod(
-	Order_ID	INT	NOT NULL	AUTO_INCREMENT,
+	Order_ID	INT	NOT NULL,
 	prod_ID		INT	NOT NULL,
 	amount		INT	NOT NULL,
-	price	DECIMAL(8,2)	NOT NULL,
+	price	DECIMAL(8,2)	NOT NULL, 
 	PRIMARY KEY (Order_ID, prod_ID),
 	FOREIGN KEY (Order_ID) REFERENCES Order_Info(Order_ID),
 	FOREIGN KEY (prod_ID) REFERENCES Inventory (Num)
 );
+
+CREATE TABLE Weights(
+	id	INT	NOT NULL	AUTO_INCREMENT,
+	low	INT,
+	high	INT,
+	cost	DECIMAL(8,2),
+	PRIMARY KEY (ID)
+);
+
+INSERT INTO Weights VALUES('1','0','2','.99');
+INSERT INTO Weights(low, high, cost) VALUES('2','5','1.99');
+INSERT INTO Weights(low, high, cost) VALUES('5','10','5.99');
+INSERT INTO Weights(low, high, cost) VALUES('10','20','9.99');
+INSERT INTO Weights(low, high, cost) VALUES('20','50','19.99');
+INSERT INTO Weights(low, high, cost) VALUES('50','100','25.99');
+INSERT INTO Weights(low, cost) VALUES('100','49.99');
+

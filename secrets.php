a1ad39102e3aac1cde4e3c74fb618ddeb7ca9858
Alexandra Alcantar <z1893793@hopper.cs.niu.edu>
2022-05-03 09:36:56 -0500

    alex

--- secrets.php
+++ secrets.php
@@ -2,14 +2,14 @@
     $username = "student";
     $password = "student";
 
-    $username2 = "z1892587";
-    $password2 = "2002Jul30";
+    $username2 = "z1893793";
+    $password2 = "2002Jan31";
 ?>
 
 <?php
     $username = "student";
     $password = "student";
 
-    $username2 = "your_z#";
-    $password2 = "your_password";
+    $username2 = "z1892587";
+    $password2 = "2002Jul30";
 ?>

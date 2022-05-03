a0ded029eb5fafcbb63b4598bf3ee4d1963cef3c
Aksh Patel <z1897952@hopper.cs.niu.edu>
2022-05-03 10:41:14 -0500

    php secrets update

--- secrets.php
+++ secrets.php
@@ -2,6 +2,6 @@
     $username = "student";
     $password = "student";
 
-    $username2 = "z1897952";
-    $password2 = "2002Jun08";
+    $username2 = "z1892587";
+    $password2 = "2002Jul30";
 ?>

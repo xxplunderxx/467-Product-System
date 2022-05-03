4bd3cd00a0cdf38799337ba987f49d768ae1a4aa
xxplunderxx <71054045+xxplunderxx@users.noreply.github.com>
2022-05-03 00:54:03 -0500

    final tables

--- new_database.sql
+++ new_database.sql
@@ -5,6 +5,8 @@
 DROP TABLE IF EXISTS Order_Prod;
 DROP TABLE IF EXISTS Order_Info;
 DROP TABLE IF EXISTS Inventory;
+DROP TABLE IF EXISTS User;
+DROP TABLE IF EXISTS Weights;
 
 CREATE TABLE Inventory(
 	Num	INT	NOT NULL	 AUTO_INCREMENT,
@@ -40,9 +42,6 @@ CREATE TABLE User(
 	status    CHAR(16) DEFAULT "default",
 	PRIMARY KEY (User_name)
 );
-#upgrade user status
-#fill in ? with username 
-# UPDATE User SET status = 'worker' WHERE User_name = '?';
 
 CREATE TABLE Weights(
 	id	INT	NOT NULL	AUTO_INCREMENT,
